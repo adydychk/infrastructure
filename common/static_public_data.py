c70b6b2d4d1e0d6f1c5ae9585088fd26386c22c4
Eugene Ponomarev <eugene.ponomarev@intel.com>
2019-04-19 15:30:49 +0300

    Use VCDP fork instead driver repo

--- common/static_public_data.py
+++ common/static_public_data.py
@@ -37,7 +37,7 @@ REPOSITORIES = {
     # open source product configs
     'product-configs': 'https://github.com/Intel-Media-SDK/product-configs.git',
     # open source media-driver
-    'media-driver': 'https://github.com/intel/media-driver.git',
+    'media-driver': 'https://github.com/VCDP/media-driver.git',
     # open source gmmlib
     'gmmlib': 'https://github.com/intel/gmmlib.git',
     # open source opencl_runtime
